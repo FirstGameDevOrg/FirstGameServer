713fac2737a2f98e10ede0aa6b970c741c4de40d
Huan Zhou <1031849267@qq.com>
2022-08-23 01:27:31 +0800

    init repository

---
+++ src/main/java/com/game/firstgameserver/FirstGameServerApplication.java
@@ -0,0 +1,13 @@
+package com.game.firstgameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGameServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstGameServerApplication.class, args);
+    }
+
+}
