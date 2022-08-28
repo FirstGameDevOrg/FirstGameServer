906c5fee98e5cada54e59646e2eb93841c876ffa
Huan Zhou <1031849267@qq.com>
2022-08-28 23:53:33 +0800

    [feat]测试自动集成功能6

--- src/main/java/com/game/firstgameserver/FirstGameServerApplication.java
+++ src/main/java/com/game/firstgameserver/FirstGameServerApplication.java
@@ -20,7 +20,7 @@ public class FirstGameServerApplication {
 
         // 注意，这个是临时测试用的，设置为 false 表示不用登录就可以访问逻辑服的方法
         ExternalGlobalConfig.verifyIdentity = false;
-        
+
         // 游戏对外服端口
         int port = 10100;
 
