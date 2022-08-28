9900f6e6ea36198c8c2ee8d85fb1d6aabe2a8402
Huan Zhou <1031849267@qq.com>
2022-08-28 21:47:49 +0800

    [feat]调整配置，取消数据库

--- src/main/java/com/game/firstgameserver/FirstGameServerApplication.java
+++ src/main/java/com/game/firstgameserver/FirstGameServerApplication.java
@@ -6,11 +6,13 @@ import com.iohao.game.bolt.broker.client.external.config.ExternalGlobalConfig;
 import com.iohao.game.simple.SimpleHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class FirstGameServerApplication {
 
     public static void main(String[] args) {
