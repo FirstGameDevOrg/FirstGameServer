acd1925203f7016445b3886b1c7768a04cbb52a2
Huan Zhou <1031849267@qq.com>
2022-08-31 23:50:55 +0800

    [feat]添加测试首页的时间

--- src/main/java/com/game/firstgameserver/controller/HelloController.java
+++ src/main/java/com/game/firstgameserver/controller/HelloController.java
@@ -3,6 +3,9 @@ package com.game.firstgameserver.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 public class HelloController {
     @RequestMapping("say")
@@ -12,6 +15,8 @@ public class HelloController {
 
     @RequestMapping("")
     public String index() {
-        return "这是测试页面";
+        Date day = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return "这是测试页面" + df.format(day);
     }
 }
